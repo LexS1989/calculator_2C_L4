3aa4a2817820be5805105a20b2e7b5ce8e61b5ca
lexS1989 <lexshadrin@mail.ru>
2022-06-19 11:17:26 +1100

    Первый коммит

---
+++ src/main/java/pro/sky/calculator_2c_lesson_4/Calculator2CL4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator_2c_lesson_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator2CL4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator2CL4Application.class, args);
+    }
+
+}
